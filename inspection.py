86f220818cf9ca4776dc3e0a0d2714eb4fc9f6cc
remrama <mallett.remy@gmail.com>
2023-03-08 21:30:54 -0600

    minor

--- inspection.py
+++ inspection.py
@@ -1,5 +1,4 @@
-"""Inspect responses to both surveys.
-"""
+"""Big exploratory correlation table."""
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -8,8 +7,8 @@ import pingouin as pg
 
 import utils
 
+
 plt.rcParams["savefig.dpi"] = 600
-# plt.rcParams["interactive"] = True
 plt.rcParams["figure.constrained_layout.use"] = True
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 8
@@ -18,22 +17,27 @@ plt.rcParams["axes.labelsize"] = 8
 plt.rcParams["axes.labelsize"] = 8
 plt.rcParams["xtick.labelsize"] = 8
 plt.rcParams["ytick.labelsize"] = 8
-plt.rcParams["axes.linewidth"] = 0.8 # edge line width
+plt.rcParams["axes.linewidth"] = 0.8
 
 config = utils.load_config()
-
-
 
 # Pick columns to plot.
 columns = [
     # from initial
-    "Dream_recall", "Nightmare_recall",
-    "Lucid_recall", "LUSK",
+    "Dream_recall",
+    "Nightmare_recall",
+    "Lucid_recall",
+    "LUSK",
     # from morning
     "Multiple_attempts",
-    "Wakeup", "Wakeup_impact",
-    "Lucidity", "Nightmare", "Sleep_paralysis",
-    "Dream_LUSK", "PANAS_pos", "PANAS_neg",
+    "Wakeup",
+    "Wakeup_impact",
+    "Lucidity",
+    "Nightmare",
+    "Sleep_paralysis",
+    "Dream_LUSK",
+    "PANAS_pos",
+    "PANAS_neg",
 ]
 
 # Choose filepaths.
@@ -41,17 +45,12 @@ root_dir = Path(config["root_directory"])
 export_path_plot = root_dir / "results" / "inspection.png"
 
 # Load data.
-df, sidecar = utils.load_data_and_sidecar(trim=True)
-
-# # Drop to final sample.
-# df = df.query("Completed_part2.eq(True)"
-#     ).query("Task_completion.eq(3)")
+df, meta = utils.load_raw(trim=True)
 
 # Draw.
 n_vars = len(columns)
-figsize = (.9*n_vars, .9*n_vars)
-fig, axes = plt.subplots(nrows=n_vars, ncols=n_vars,
-    figsize=figsize, sharex="col", sharey=False)
+figsize = (0.9*n_vars, 0.9*n_vars)
+fig, axes = plt.subplots(nrows=n_vars, ncols=n_vars, figsize=figsize, sharex="col", sharey=False)
 
 def get_bins(var):
     if "LUSK" in var:
@@ -59,10 +58,10 @@ def get_bins(var):
     elif "PANAS" in var:
         levels = range(10, 51)
     else:
-        levels = [ int(k) for k in sidecar[var]["Levels"] ]
-    low_lim = min(levels) - .5
-    high_lim = max(levels) + .5
-    return np.linspace(low_lim, high_lim, len(levels)+1)
+        levels = [int(k) for k in meta[var]["Levels"]]
+    low_lim = min(levels) - 0.5
+    high_lim = max(levels) + 0.5
+    return np.linspace(low_lim, high_lim, len(levels) + 1)
 
 hist1d_kwargs = dict(color="white", edgecolor="black", linewidth=1, clip_on=False)
 hist2d_kwargs = dict(cmap="Blues", clip_on=False)
@@ -122,8 +121,7 @@ for c in range(n_vars):
         if r != c:
             text = f"r = {r:.2f}\np = {p:.3f}".replace("0.", ".")
             color = "black" if p < .1 else "gainsboro"
-            ax.text(.95, .05, text, color=color,
-                va="bottom", ha="right", transform=ax.transAxes)
+            ax.text(0.95, 0.05, text, color=color, va="bottom", ha="right", transform=ax.transAxes)
 
 fig.align_labels()
 
--- sample.py
+++ sample.py
@@ -1,7 +1,4 @@
-"""Describe a specific feature of the the final sample.
-
-Choose a feature to export a distribution plot and data file.
-"""
+"""Get some general summary statistics of the whole sample."""
 import argparse
 from pathlib import Path
 
@@ -26,12 +23,11 @@ config = utils.load_config()
 
 # Choose filepaths.
 root_dir = Path(config["root_directory"])
-import_path = root_dir / "derivatives" / "data_trimmed.tsv"
 export_path_data = root_dir / "derivatives" / f"sample_{feature}.tsv".lower()
 export_path_plot = export_path_data.with_suffix(".png")
 
 # Load data.
-df, sidecar = utils.load_data_and_sidecar(import_path, trim=True)
+df, meta = utils.load_raw(trim=True)
 
 # # Drop to final sample.
 # df = df.query("Completed_part2.eq(True)"
@@ -42,7 +38,7 @@ ser = df[feat].value_counts().rename("count").rename_axis(feature).sort_index()
 
 # Get full text labels.
 if feat != "Condition":
-    response_legend = { int(k): v.split(" ")[0] for k, v in sidecar[feature]["Levels"].items() }
+    response_legend = {int(k): v.split(" ")[0] for k, v in meta[feature]["Levels"].items()}
     ser.index = ser.index.map(response_legend)
 
 # Draw plot.
